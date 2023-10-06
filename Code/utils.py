44a6aa8481f10ce38a52c000e9297ebae432cacd
ybbwcwaps <gjr13754822885@mail.ustc.edu.cn>
2023-10-06 17:22:37 +0800

    修改代码细节，增加注释与文档

--- Code/utils.py
+++ Code/utils.py
@@ -1,14 +1,16 @@
 import fitz
 import re
 
+
 class Pdf2Ref:
     """Extract references from pdf
     """
+
     def __init__(self, pdf_path) -> None:
         self.pdf = fitz.open(pdf_path)
         self.references = ""
-        self.ref_list=[]
-        self.title_list=[]
+        self.ref_list = []
+        self.title_list = []
         self.GetTitle()
 
     def GetRefPages(self):
@@ -17,20 +19,21 @@ class Pdf2Ref:
         ref_list = []
         find = False
         for num, p in enumerate(self.pdf):
+            # Read in blocks
             content = p.get_text("blocks")
             for pc in content:
                 if find:
                     ref_list.append(pc[4])
                 else:
                     txtblocks = pc[4]
-                    if "references" == txtblocks.lower().replace("\n", ""): # find references
+                    if "references" == txtblocks.lower().replace("\n", ""):  # find references
                         find = True
-        
-        ref_list=[i.replace('\n',' ') for i in ref_list]
-        references=' '.join(ref_list).replace('- ','-') # This is followed by a word wrap
+
+        ref_list = [i.replace('\n', ' ') for i in ref_list]
+        references = ' '.join(ref_list).replace(
+            '- ', '-')  # This is followed by a word wrap
         self.pdf.close()
         self.references = references
-    
 
     def GetUnitRef(self):
         """Parse the text content into a single reference.
@@ -40,11 +43,10 @@ class Pdf2Ref:
             pattern = re.compile(r'\[[1-9][0-9]*\]\s')
         else:
             pattern = re.compile(r'[1-9][0-9]*\.\s')
-            
-        ref_list=re.split(pattern,self.references)
-        ref_list=list(filter(None,ref_list))
+
+        ref_list = re.split(pattern, self.references)
+        ref_list = list(filter(None, ref_list))   # Filter empty string
         self.ref_list = ref_list
-    
 
     def GetTitle(self):
         """Get title form every reference.
@@ -58,13 +60,13 @@ class Pdf2Ref:
                 f = f.replace(',', ' ')
                 pattern = r'\.(?=\d)'
                 f = re.sub(pattern, ' ', f)
-                match = re.search(r'(https?|ftp)://[^\s/$.?#].[^\s]*', f) 
+                match = re.search(r'(https?|ftp)://[^\s/$.?#].[^\s]*', f)
                 self.title_list.append(match.group())
                 continue
 
-            keywords = ["\.","\?", "!", r'\([0-9]+\)']
+            keywords = ["\.", "\?", "!", r'\([0-9]+\)']
             pattern = r"(" + r"|".join(keywords) + r")"
             sentences = re.split(pattern, f)
-            # 几种情况：开头结尾空格，中间的换行符，结尾数字
-            self.title_list.append(re.sub(r'[\d\s]*$', '', (re.sub(r'\s+', ' ', sentences[2][:].replace('\n', ' ').lstrip().rstrip())) + "  "))
-        
+            # Several cases: beginning and ending Spaces, middle line breaks, and ending numbers
+            self.title_list.append(re.sub(
+                r'[\d\s]*$', '', (re.sub(r'\s+', ' ', sentences[2][:].replace('\n', ' ').lstrip().rstrip())) + "  "))
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PDF2Bib
 从PDF中解析出所有的参考文献，显示出来，并下载所有的文献的 BIB 文件
+
+```
+.   
+├── README.md
+├── Code
+│   ├── background_rc.py
+│   ├── main.py
+│   ├── Ui_interface.py
+│   ├── utils.py
+├── resources
+│   ├── background.jpg
+│   ├── background.qrc
+|	├── icon.ico
+├── example
+│   ├── *.pdf	# some pdf for test
+├── run.bat
+├── requirements.txt
+```
+
+运行，需要的python库在[requirements.txt](./requirements.txt)中，并注意环境变量配置。
+
+双击`run.bat`即可运行。
